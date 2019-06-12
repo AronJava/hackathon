60474caad0e1d1fa513ab4515edb1b0fe3e1d775
sherlock-wong <sux_wang@163.com>
2019-06-12 12:15:05 +0800

    ceshi

--- marathon/main.go
+++ marathon/main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/skip2/go-qrcode"
 
 func main() {
-	qrcode.WriteFile("http://188.131.146.10:8089/", qrcode.Medium, 256, "./qr/baidu_qrcode.png")
+	qrcode.WriteFile("http://188.131.146.10:8089/", qrcode.Medium, 256, "./qr/baidu_qr.png")
 }
