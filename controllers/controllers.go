db349534bde861afefde3467bfb3eae34e5895ed
chenjiachen <chenjiachen@ucfgroup.com>
2019-06-14 21:36:36 +0800

    23

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -6,6 +6,7 @@ import (
 	"hackathon/controllers/bulletscreen"
 	"hackathon/controllers/admin"
 	"hackathon/controllers/account"
+	"hackathon/controllers/paper"
 )
 
 // Routers 路由
@@ -14,13 +15,13 @@ var Routers = map[string]http.HandlerFunc{
 	// websocket 弹幕接口
 	"/bulletscreen/ws": bulletscreen.Ws,
 	// 帖子展示
-	"/paper/show": Paper.Show,
+	"/paper/show": paper.Show,
 	// 添加评论
-	"/paper/addComment": Paper.addComment,
+	"/paper/addComment": paper.addComment,
 	// 报名
-	"/paper/signup": Paper.Signup,
+	"/paper/signup": paper.Signup,
 	// 个人信息
-	"/admin/index": Admin.Index,
+	"/admin/index": admin.Index,
 	//我的贴子
 	"/admin/paper": admin.Paper,
 	//我的收藏
