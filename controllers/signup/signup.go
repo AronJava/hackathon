c7db67a4ae98b8cee17a725d128f499a585e73c8
sherlock-wong <sux_wang@163.com>
2019-06-14 19:22:00 +0800

    签到

--- controllers/signup/signup.go
+++ controllers/signup/signup.go
@@ -1,8 +1,28 @@
 package signup
 
-import "net/http"
+import (
+	"hackathon/modules/signup"
+	"hackathon/utils"
+	"log"
+	"net/http"
+	"strconv"
+)
 
 // Signup 签到
 func Signup(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	userID, err := strconv.Atoi(r.Header.Get("User-Id"))
+	if userID <= 0 {
+		utils.ResponseJson(w, -1, "获取用户登录状态失败", nil)
+		log.Printf("update get userID failed. err:%s", err)
+		return
+	}
+	eventID, err := strconv.Atoi(r.Form.Get("eventID"))
+	if err != nil {
+		utils.ResponseJson(w, -1, "param error", nil)
+		return
+	}
+	signup.CheckIn(userID, eventID)
+	utils.ResponseJson(w, 0, "签到成功", nil)
 
 }
