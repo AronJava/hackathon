cc0a6682a159e7f1991e62190bc0cc21cb680fd4
sherlock-wong <sux_wang@163.com>
2019-06-14 14:29:48 +0800

    添加接口

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -6,7 +6,17 @@ import (
 	"hackathon/controllers/index"
 )
 
+// Routers 路由
 var Routers = map[string]http.HandlerFunc{
-	"/":   index.Index,
+	"/": index.Index,
+	// websocket 弹幕接口
 	"/ws": index.Ws,
+	// 帖子展示
+	"/paper/show": Paper.Show,
+	// 添加评论
+	"/paper/addComment": Paper.addComment,
+	// 报名
+	"/paper/signup": Paper.Signup,
+	// 个人信息
+	"/admin/personalinfo": Admin.PersonalInfo,
 }
